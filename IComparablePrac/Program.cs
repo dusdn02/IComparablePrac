2ab0cb6df2f62c86e5427064bf8193c6ebaf3d82
dusdn02 <lyw9114@naver.com>
2020-07-01 14:07:27 +0900

    초기 커밋.

---
+++ IComparablePrac/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparablePrac
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            // throw new NotImplementedException();
+            //return this.Price - ((Product)obj).Price;
+            //return this.Name[0] - ((Product)obj).Name[0];
+            return this.Price.CompareTo(((Product)obj).Price);
+        }
+
+        public override string ToString()
+        {
+            return Name + " : " + Price + "원";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>()
+            {
+                new Product(){ Name="고구마",Price=1000 },
+                new Product(){ Name="사과",Price=1500 },
+                new Product(){ Name="바나나",Price=2000 },
+                new Product(){ Name="감자",Price=3000 },
+            };
+            list.Sort();
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
